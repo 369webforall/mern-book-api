e5ca5d532f79ca07bb3ed8e58697afbea4fc15dd
kabitaremotework <104657984+kabitaremotework@users.noreply.github.com>
2023-08-04 13:35:51 +0545

    update

---
+++ frontend/src/pages/AddBook.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+
+const AddBook = () => {
+  
+  return (
+    <div>
+      <div>
+        <h1 className="text-center text-2xl">Add Book</h1>
+        <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name" className="block font-medium text-gray-800">
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={book.name}
+              onChange={handleChange}
+              className="mt-1 p-2 border border-gray-500 rounded w-full required"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="author" className="block font-medium text-gray-800">
+              Author:
+            </label>
+            <input
+              type="text"
+              id="author"
+              name="author"
+              onChange={handleChange}
+              value={book.author}
+              className="mt-1 p-2 border border-gray-500 rounded w-full required"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="desc" className="block font-medium text-gray-800">
+              Description
+            </label>
+            <input
+              type="text"
+              id="desc"
+              name="desc"
+              onChange={handleChange}
+              value={book.desc}
+              className="mt-1 p-2 border border-gray-500 rounded w-full required"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="price" className="block font-medium text-gray-800">
+              Price
+            </label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              onChange={handleChange}
+              value={book.price}
+              className="mt-1 p-2 border border-gray-500 rounded w-full required"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="image" className="block font-medium text-gray-800">
+              Image:
+            </label>
+            <input
+              type="text"
+              id="image"
+              name="img"
+              value={book.img}
+              onChange={handleChange}
+              className="mt-1 p-2 border border-gray-500 rounded w-full required"
+            />
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="available"
+              name="available"
+              checked={checked}
+              onChange={() => setChecked(!checked)}
+            />
+            <label htmlFor="available">Available</label>
+          </div>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="px-4 py-2 bg-violet-600 text-white rounded hover:bg-violet-800"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddBook;
